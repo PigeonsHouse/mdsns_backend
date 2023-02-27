817eae096c9f60375e06ccd8d1b28880db33eca0
t3mp-0xCC <40132263+t3mp-0xCC@users.noreply.github.com>
2023-02-27 12:58:31 +0900

    add: logger

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
-use dotenvy::dotenv;
 use actix_web::{App, HttpServer};
+use actix_web::middleware::Logger;
+use dotenvy::dotenv;
+use env_logger::Env;
 use mdsns_backend::routers;
 mod auth;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
     HttpServer::new(|| {
         App::new()
+            .wrap(Logger::default())
     })
         .bind(("127.0.0.1", 8080))?
         .run()
