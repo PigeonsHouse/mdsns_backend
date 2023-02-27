ab178307dd160460b492fac26dcfb41a99950d6a
t3mp-0xCC <40132263+t3mp-0xCC@users.noreply.github.com>
2023-02-27 18:33:36 +0900

    add: middleware auth perfectly

--- src/models.rs
+++ src/models.rs
@@ -1,12 +1,23 @@
 use serde::Serialize;
 use chrono::NaiveDateTime;
 use diesel::prelude::{Insertable, Queryable};
+use crate::schema::users;
 
 #[derive(Queryable, Serialize)]
 pub struct User {
     pub description: Option<String>,
     pub id: String,
     pub name: String,
+    pub email: String,
     pub created_at: NaiveDateTime,
     pub updated_at: NaiveDateTime
 }
+
+#[derive(Insertable)]
+#[diesel(table_name = users)]
+pub struct NewUser<'a> {
+    pub id: &'a String,
+    pub name: &'a String,
+    pub email: &'a String,
+}
+
