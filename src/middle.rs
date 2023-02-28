a89b31d0616ec4acd1967f783802292a84a509db
t3mp-0xCC <40132263+t3mp-0xCC@users.noreply.github.com>
2023-02-28 13:33:33 +0900

    Rust nooooob

--- src/middle.rs
+++ src/middle.rs
@@ -22,9 +22,31 @@ pub struct AuthRequest {
 
 pub async fn oauth_google(req: &HeaderValue) -> Result<bool, Error>{
     debug!("Started Google OAuth 2.0 modoki !");
+    let api_key: String = std::env::var("FIREBASE_API").expect("FIREBASE_API does not exist !");
+    let auth = FireAuth::new(api_key);
     match validate_token(req.to_str().unwrap()).await {
         Ok(res) => {
             if res {
+                /* Rusting !!! */
+                // Exist on firebase check
+                let user_local_id = match auth.get_user_info(req.to_str().unwrap()).await {
+                    Ok(user) => user.local_id,
+                    Err(_) => return Err(actix_web::error::ErrorInternalServerError("sushi!")),
+                };
+                // Exist on local db check
+                debug!("local_id: {:?}", user_local_id);
+                let mut conn = establish_connection();
+                match search_user_from_db(&mut conn, user_local_id.clone()) {
+                    true => (),
+                    false => {
+                        // copy UserData from Firebase
+                        let user_info = match auth.get_user_info(req.to_str().unwrap()).await {
+                            Ok(user) => user,
+                            Err(_) => return Err(actix_web::error::ErrorInternalServerError("sushi!")),
+                        };
+                        register_user(&mut conn, &user_info.local_id, &user_info.local_id, &user_info.email);
+                    },
+                };
                 Ok(res)
             } else {
                 Err(actix_web::error::ErrorInternalServerError("kani!"))
